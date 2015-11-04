e8f0a6980217e68e09710a77e0be28dbf76a3c26
Nathan Shively-Sanders <nathansa@microsoft.com>
2015-11-04 09:20:01 -0800

    Accept baselines (parsing succeeds, at least)

---
+++ tests/baselines/reference/tupleKinds.js
@@ -0,0 +1,14 @@
+//// [tupleKinds.ts]
+function tuple<...T>(...args:...T) {
+    return args;
+}
+
+
+//// [tupleKinds.js]
+function tuple() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i - 0] = arguments[_i];
+    }
+    return args;
+}
